cd2ae3cadf743a4e3ff46499d5b2655709817f86
jhw2167 <jhw2167@columbia.edu>
2022-03-16 11:48:41 -0500

    synthesizing VENDOR_TABLE update into trigger, an arduos process

--- src/main/resources/data/schema.sql
+++ src/main/resources/data/schema.sql
@@ -248,6 +248,19 @@ FROM (
 ) a
 WHERE a.T_ID = T.T_ID;
 
+--- ADD TIMESTAMP to TRANSACTIONS ---
+
+CREATE TABLE TRANSACTIONS_22_3_14 AS (SELECT * FROM TRANSACTIONS);
+SELECT * FROM TRANSACTIONS_22_3_14
+
+ALTER TABLE TRANSACTIONS 
+ADD timestamp_ TIMESTAMP DEFAULT NOW()
+
+SELECT * FROM TRANSACTIONS T
+
+UPDATE TRANSACTIONS T
+SET timestamp_ = T.PURCHASE_DATE::timestamp
+
 
 ---------------------------- VENDOR TABLE TOUR ----------------------------
 
@@ -364,19 +377,133 @@ SELECT * FROM VENDORS V
 --- CREATE TRIGGER FOR ADJUSTING VENDORS TABLE ---
 
 	-- first write function
-DROP VIEW test;
+DROP table test;
+CREATE TABLE test(
+myBool varchar(255));
 
-CREATE FUNCTION update_vendors(upd_vend STRING, )
-	RETURNS void AS
+INSERT INTO test VALUES ('test');
+SELECT * FROM test;
+SELECT * FROM VENDORS V ;
+DELETE FROM test;
+
+SELECT vendor FROM TRANSACTIONS T ORDER BY timestamp_ DESC LIMIT 1;
+
+DROP FUNCTION IF EXISTS update_vendors CASCADE;
+CREATE FUNCTION update_vendors()
+	RETURNS trigger AS
 	$$
 	BEGIN 
-		INSERT INTO test VALUES (true);		
+		DROP VIEW IF EXISTS upd;
+		CREATE VIEW upd AS SELECT vendor AS val 
+			FROM TRANSACTIONS T 
+			ORDER BY timestamp_
+			DESC LIMIT 1;
+	
+	-- DROP PREVIOUS VIEWS	
+		DROP VIEW IF EXISTS upd_vendor CASCADE;
+		DROP VIEW IF EXISTS BASE_INC cascade;
+		DROP VIEW  IF EXISTS cat_select cascade;
+
+	-- GET BASE VALUES
+		CREATE VIEW base_inc AS SELECT DISTINCT vendor AS v, 0 AS amt, 0 AS no_inc, 0 AS inc
+		FROM TRANSACTIONS T
+		WHERE VENDOR = (SELECT val FROM upd);
+	
+	-- GET CATEGORIES
+	CREATE VIEW cat_select AS SELECT * FROM (
+		SELECT CATEGORY, vendor, IS_INCOME, cnt, 
+			RANK() OVER (PARTITION BY VENDOR, IS_INCOME 
+						ORDER BY cnt DESC) AS rn
+			FROM (
+			SELECT VENDOR, CATEGORY, IS_INCOME, COUNT(*) AS cnt 
+			FROM TRANSACTIONS T
+			GROUP BY CATEGORY, VENDOR, IS_INCOME
+			) mainSubQ 
+		) outerSubQ
+		WHERE outerSubQ.rn = 1;
+	
+	
+	
+		-- CREATE VIEW OF INCOME VENDORS AND NON-INCOME VENDORS
+		CREATE VIEW inc_vends AS
+		(SELECT vendor AS vendor, MODE(amount) AS amt, COUNT(*) AS inc
+			FROM TRANSACTIONS T
+			WHERE IS_INCOME=true
+			GROUP BY VENDOR);
+		
+		SELECT * FROM inc_vends UNION (SELECT v, amt, 
+		inc FROM base_inc
+		WHERE v NOT IN (
+			SELECT vendor 
+			FROM inc_vends))
+		ORDER BY vendor;
+		
+		CREATE VIEW no_inc_vends AS SELECT vendor AS vendor, MODE(amount) AS amt, COUNT(*) AS no_inc
+		FROM TRANSACTIONS T
+		WHERE IS_INCOME=false
+		GROUP BY VENDOR;
+		
+		SELECT * FROM no_inc_vends UNION (SELECT v, amt, 
+		no_inc FROM base_inc
+		WHERE v NOT IN (
+			SELECT vendor 
+			FROM no_inc_vends))
+		ORDER BY vendor;
+		
+		-- CREATE VIEW OF MERGED VENDORS
+		CREATE VIEW v3 AS SELECT v1.vendor, v1.amt AS inc, v2.amt AS spnt,
+		v1.inc AS cnt_inc, v2.no_inc AS cnt_no_inc
+		FROM (
+			(SELECT * FROM inc_vends UNION 
+			(SELECT v, amt, inc
+			FROM base_inc
+			WHERE v NOT IN (
+				SELECT vendor 
+				FROM inc_vends))) v1 
+				
+		INNER JOIN (
+		-- Append all vendors to no_inc list 
+		SELECT * FROM no_inc_vends UNION (SELECT v, amt, 
+		no_inc FROM base_inc
+		WHERE v NOT IN (
+			SELECT vendor 
+			FROM no_inc_vends))) v2 ON v1.vendor=v2.vendor 
+		
+		);
+		
+	-- SELECT RESULTS INTO VENDORS TABLE
+	CREATE VIEW upd_vend AS (SELECT v3.vendor AS vendor, category,
+			 (CASE WHEN cnt_inc > cnt_no_inc THEN inc ELSE spnt END) AS amount,
+			 (cnt_inc > cnt_no_inc) AS is_typically_inc
+		FROM v3 
+		INNER JOIN
+		cat_select AS cs ON v3.VENDOR=cs.vendor
+		WHERE (cnt_inc > cnt_no_inc AND is_income) OR (cnt_inc <= cnt_no_inc AND NOT is_income)
+		ORDER BY v3.VENDOR);
+	
+	IF EXISTS (SELECT * FROM VENDORS V WHERE VENDOR=v)
+		THEN
+			UPDATE VENDORS 
+			SET vendor=upd_vend.vendor, 
+				AMOUNT=upd_vend.amount,
+				CATEGORY=upd_vend.category,
+				IS_TYPICALLY_INCOME=upd_vend.is_typically_income
+			WHERE vendor=upd_vend.vendor;
+		ELSE 
+			INSERT INTO VENDORS (vendor, AMOUNT, CATEGORY, IS_TYPICALLY_INCOME)
+			VALUES (upd_vend.vendor, upd_vend.amount, upd_vend.category, upd_vend.is_typically_income);
+		END IF;
+	
 		
 	END
 	$$ LANGUAGE plpgsql;
-	
+
+CREATE TRIGGER VEND_UPD_ON_TRANS_INS AFTER INSERT 
+	ON test EXECUTE PROCEDURE update_vendors();
 
 
+
+DROP TRIGGER VEND_UPD_ON_TRANS_INS;
 ---------------- END TRIGGER ----------------
 
 
